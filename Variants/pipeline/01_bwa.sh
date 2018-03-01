9f1d0c570ff53e8408272235961c140e7697c215
smasonjones <smaso003@ucr.edu>
2018-03-01 12:00:51 -0800

    Update 01_bwa.sh
    Added annotation

--- Variants/pipeline/01_bwa.sh
+++ Variants/pipeline/01_bwa.sh
@@ -21,10 +21,10 @@ CPU=1
 SAMPLEFILE=samples.info
 
 BWA=bwa
-BASE=/bigdata/stajichlab/shared/projects/Candida/Clus_reseq
+BASE=/bigdata/stajichlab/shared/projects/Candida/Clus_reseq #PATH/TO/working directory
 TEMPDIR=/scratch # this might need to switch
-GENOMEIDX=$BASE/genome/candida_lusitaniae_ATCC42720_w_CBS_6936_MT.fasta
-OUTPUT=bam
+GENOMEIDX=$BASE/genome/candida_lusitaniae_ATCC42720_w_CBS_6936_MT.fasta #PATH/TO/genone
+OUTPUT=bam #Defaults to outputing to bam folder. If you change this, you will need to change input folder in later scripts in later 
 
 hostname
 mkdir -p $OUTPUT
@@ -44,22 +44,22 @@ if [ ! $LINE ]; then
  exit
 fi
 
-ROW=`head -n $LINE $SAMPLEFILE | tail -n 1`
+ROW=`head -n $LINE $SAMPLEFILE | tail -n 1` 
 
-sed -n ${LINE}p $SAMPLEFILE | while read SAMPLE READ1 READ2
+sed -n ${LINE}p $SAMPLEFILE | while read SAMPLE READ1 READ2 #assigns sample name and read files paths from sample.info.
 do
- INDIR=$(dirname $READ1)
+ INDIR=$(dirname $READ1) 
  echo "SAMPLE=$SAMPLE"
- if [ ! -f $OUTPUT/$SAMPLE.DD.bam ]  || [ ! -s $OUTPUT/$SAMPLE.DD.bam ]; then
-  if [ ! -f $OUTPUT/$SAMPLE.bwa.bam ]; then
-   $BWA mem -M -t $CPU $GENOMEIDX $READ1 $READ2 | samtools view -T $GENOMEIDX -b -o $OUTPUT/$SAMPLE.bwa.bam -
+ if [ ! -f $OUTPUT/$SAMPLE.DD.bam ]  || [ ! -s $OUTPUT/$SAMPLE.DD.bam ]; then #checks for exisiting output
+  if [ ! -f $OUTPUT/$SAMPLE.bwa.bam ]; then #checks for exisiting output
+   $BWA mem -M -t $CPU $GENOMEIDX $READ1 $READ2 | samtools view -T $GENOMEIDX -b -o $OUTPUT/$SAMPLE.bwa.bam - #bwa alignment of reads v reference genome. Outputs to file named by sample in OUTDIR Folder
   fi
-  if [ ! -f $OUTPUT/$SAMPLE.RG.bam ]; then
-   java -jar $PICARD AddOrReplaceReadGroups I=$OUTPUT/$SAMPLE.bwa.bam O=$OUTPUT/$SAMPLE.RG.bam RGLB=$SAMPLE RGID=$SAMPLE RGSM=$SAMPLE RGPL=Illumina RGPU=$SAMPLE RGCN=CHUV RGDS="$READ1 $READ2" CREATE_INDEX=true SO=coordinate TMP_DIR=$TEMPDIR
+  if [ ! -f $OUTPUT/$SAMPLE.RG.bam ]; then #checks for exisiting output
+   java -jar $PICARD AddOrReplaceReadGroups I=$OUTPUT/$SAMPLE.bwa.bam O=$OUTPUT/$SAMPLE.RG.bam RGLB=$SAMPLE RGID=$SAMPLE RGSM=$SAMPLE RGPL=Illumina RGPU=$SAMPLE RGCN=CHUV RGDS="$READ1 $READ2" CREATE_INDEX=true SO=coordinate TMP_DIR=$TEMPDIR #Assigns Read Groups using Picard
    # rm $OUTPUT/$SAMPLE.sam
    # touch $OUTPUT/$SAMPLE.sam
   fi
 
-  java -jar $PICARD MarkDuplicates I=$OUTPUT/$SAMPLE.RG.bam O=$OUTPUT/$SAMPLE.DD.bam METRICS_FILE=$SAMPLE.dedup.metrics CREATE_INDEX=true VALIDATION_STRINGENCY=SILENT
+  java -jar $PICARD MarkDuplicates I=$OUTPUT/$SAMPLE.RG.bam O=$OUTPUT/$SAMPLE.DD.bam METRICS_FILE=$SAMPLE.dedup.metrics CREATE_INDEX=true VALIDATION_STRINGENCY=SILENT #Marks Duplicate reads using Picard
  fi
 done
